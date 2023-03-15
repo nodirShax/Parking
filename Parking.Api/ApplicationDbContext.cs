df348599a8c7424f2887813eade72685f1f8519d
nodirShax <burxonovnodirbek8@gmail.com>
2023-03-15 06:02:04 -0700

    Fix AppSetting and create ModelDb

---
+++ Parking.Api/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Parking.Api
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<ParkModel> ParkModels { get; set; }
+    }
+}
---
+++ Parking.Api/ParkModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Parking.Api
+{
+    public class ModelDb
+    {
+        public int Id { get; set; }
+        public string NumberCar { get; set; }
+        public string EnterTime { get; set; }
+        public string LeftTime { get; set; }
+    }
+}
